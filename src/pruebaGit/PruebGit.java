98fc654832fa9c7b4709f40f4f9551c4fd661998
Elias Wassit <ewascal004@g.educaand.es>
2024-11-19 09:44:59 +0100

    Incluyo un condicional

--- src/pruebaGit/PruebGit.java
+++ src/pruebaGit/PruebGit.java
@@ -3,7 +3,11 @@ package pruebaGit;
 public class PruebGit {
 
 	public static void main(String[] args) {
+		int numero = 0;
 		System.out.println("hello world");
 		System.out.println("bienvenido");
+		if (numero == 0) {
+			System.out.println("el nuemro es 0");
+		}
 	}
 }
